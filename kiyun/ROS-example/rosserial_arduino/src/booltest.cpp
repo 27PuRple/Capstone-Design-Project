18b810d1fa25c7baec79167524378213337528f0
27PuRple <44526808+27PuRple@users.noreply.github.com>
2020-04-12 23:04:12 +0900

    Update booltest.cpp

--- kiyun/ROS-example/rosserial_arduino/src/booltest.cpp
+++ kiyun/ROS-example/rosserial_arduino/src/booltest.cpp
@@ -2,4 +2,18 @@
 #include "std_msgs/Bool.h"
 #include <iostream>
 
+void boolcallback(const std_msgs::Bool::ConstPtr& msg) {
+  ROS_INFO("");
+}
 
+int main(int argc, char** argv) {
+  
+  ros::init(argc, argv, "booltest");
+  
+  ros::NodeHandle nh;
+  ros::Subscriber sub = nh.subscribe("/pushed", 1000, boolcallback);
+  
+  ros::spin();
+  
+  return 0;
+}
